10c6640973b6e4d56de47a068c12cc480f512b30
ValentinAtomicDigital <vboutouria@atomicdigital.design>
2026-04-01 12:38:54 +0200

    ecoute DEBUG

--- ServerOSCPython.py
+++ ServerOSCPython.py
@@ -1,19 +1,19 @@
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-# Fonction appelée à la réception du message
-def recevoir_valeur(unused_addr, args, valeur):
-    print("Valeur reçue de PC B :", valeur)
+# Fonction appelée pour TOUS les messages
+def print_all(address, *args):
+    print(f"{address} | Args: {args}")
 
-# Création d’un dispatcher pour écouter l’adresse /valeur
 disp = dispatcher.Dispatcher()
-disp.map("/velo_start", recevoir_valeur, "velo_start")
 
-# IP et port sur lesquels PC A écoute
-ip = "0.0.0.0"   # Écoute sur toutes les interfaces réseau
-port = 8001    # Doit correspondre au port choisi dans Unreal
+# Handler par défaut (catch-all)
+disp.set_default_handler(print_all)
+
+ip = "0.0.0.0"
+port = 9000
 
 server = osc_server.ThreadingOSCUDPServer((ip, port), disp)
-print("Serveur OSC en écoute sur {}:{}".format(ip, port)) 
+print(f"Serveur OSC en écoute sur {ip}:{port}")
 
 server.serve_forever()
